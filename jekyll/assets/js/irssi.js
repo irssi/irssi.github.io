251f6d9f4e8a99c902affc34f510322fcd978e2e
Nicolas Kuttler <git@kuttler.eu>
2015-02-12 17:29:19 +0100

    Remove comments

--- jekyll/assets/js/irssi.js
+++ jekyll/assets/js/irssi.js
@@ -2,14 +2,11 @@ function initMenu() {
     'use strict';
     var toggle = document.getElementById("navbar-toggle"),
         menu = document.getElementById("navbar");
-    // IE9+
-    toggle.addEventListener('click', function () {
-        // IE10+
+    toggle.addEventListener('click', function (event) {
         menu.classList.toggle("collapse");
     });
 }
 
-// IE9+
 document.addEventListener("DOMContentLoaded", function(event) {
     initMenu();
 });
