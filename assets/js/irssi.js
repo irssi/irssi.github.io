85cce67ca112b954cc49fe314f3028581bd7a727
Nicolas Kuttler <git@kuttler.eu>
2015-02-12 15:46:10 +0100

    Remove jQuery and bootstrap js
    We don't really need any of it (for now at least) as we only need to
    show/hide the mobile navigation.

---
+++ assets/js/irssi.js
@@ -0,0 +1,15 @@
+function initMenu() {
+    'use strict';
+    var toggle = document.getElementById("navbar-toggle"),
+        menu = document.getElementById("navbar");
+    // IE9+
+    toggle.addEventListener('click', function () {
+        // IE10+
+        menu.classList.toggle("collapse");
+    });
+};
+
+// IE9+
+document.addEventListener("DOMContentLoaded", function(event) {
+    initMenu();
+});
