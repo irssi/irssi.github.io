426fd91dcdfb5bd270ef1a185ef4face49bda1bf
Ailin Nemui <ailin@z30a.localdomain>
2018-01-25 09:54:34 +0100

    use less js

--- assets/js/irssi.js
+++ assets/js/irssi.js
@@ -1,16 +1,3 @@
-function initMenu() {
-    'use strict';
-    var toggle = document.getElementById("navbar-toggle"),
-        menu = document.getElementById("navbar");
-    toggle.addEventListener('click', function (event) {
-        menu.classList.toggle("in");
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    initMenu();
-});
-
 // http://stackoverflow.com/a/34003331
 if (Element.prototype.scrollIntoView && !Element.prototype.scrollIntoViewIfNeeded) {
     Element.prototype.scrollIntoViewIfNeeded = function (centerIfNeeded) {
