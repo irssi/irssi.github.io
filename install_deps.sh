014d5291154ad747f9fe10153fc592b9b6f97763
Ailin Nemui <ailin@d5421s.localdomain>
2022-06-12 04:13:11 +0200

    fix canonical url bug

--- install_deps.sh
+++ install_deps.sh
@@ -2,6 +2,7 @@
 set -e
 
 pip install -r sphinx/requirements.txt
+patch -d "$(python -c 'import sphinx.builders.html;print(sphinx.builders.html.__path__[0])')" -p4 < patches/9731.patch
 pip install -e ./furo
 cpm install --cpanfile=./irssi-website-tools/_tools/cpanfile
 perl irssi-website-tools/_tools/issues_titles.pl
