3f570bf3976fc857f6d3401ee5cda9fb1831f981
Ailin Nemui <ailin@d5421s.localdomain>
2022-06-17 15:41:57 +0200

    fix clone script

--- clone.sh
+++ clone.sh
@@ -3,8 +3,8 @@ set -e
 
 github=https://github.com
 [ -d irssi ]               || git clone $github/irssi/irssi
-[ -d sphinx ]              || git clone --no-local . sphinx -b sphinx
-[ -d irssi.github.io ]     || git clone --no-local . irssi.github.io -b master
+[ -d sphinx ]              || git worktree add sphinx sphinx
+[ -d irssi.github.io ]     || git worktree add irssi.github.io master
 [ -d furo ]                || git clone $github/ailin-nemui/furo
 [ -d irssi-website-tools ] || git clone $github/ailin-nemui/irssi-website-tools
 [ -d Ascidia ]             || git clone $github/ailin-nemui/Ascidia
