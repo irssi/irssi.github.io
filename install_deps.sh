807ae3f74596c0fe87ed910b795862dfae468197
Ailin Nemui <ailin@d5421s.localdomain>
2023-03-28 18:05:53 +0200

    use python -m pip

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-pip install -r sphinx/requirements.txt
+python -m pip install -r sphinx/requirements.txt
 
 patch --no-backup-if-mismatch -r - -R -f -d "$(python -c 'import sphinx.builders.html;print(sphinx.builders.html.__path__[0])')" -p4 < patches/9731.patch || :
 patch -d "$(python -c 'import sphinx.builders.html;print(sphinx.builders.html.__path__[0])')" -p4 < patches/9731.patch
 
-pip install -e ./furo
+python -m pip install -e ./furo
 
 PERL5LIB="$(pwd)/local/lib/perl5${PERL5LIB+:}$PERL5LIB"
 export PERL5LIB
